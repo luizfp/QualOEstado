f3d53dde26c6df151ecba73132fb8651441650ef
Luiz Felipe <luiz.felipe_95@hotmail.com>
2015-09-22 23:19:49 -0300

    Design melhorado

--- app/src/main/java/br/com/lfpmobile/qualoestado/activities/ActMain.java
+++ app/src/main/java/br/com/lfpmobile/qualoestado/activities/ActMain.java
@@ -1,21 +1,52 @@
 package br.com.lfpmobile.qualoestado.activities;
 
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.TextView;
 
 import br.com.lfpmobile.qualoestado.R;
 
 public class ActMain extends AppCompatActivity {
 
+    private TextView txtPontosJogadorMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+
+        txtPontosJogadorMenu = (TextView)findViewById(R.id.txtPontosJogadorMenu);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startCountAnimation();
+    }
+
+    //FIXME
+    // Funciona apenas da API 3.0 (HONEYCOMB) em diante.
+    @SuppressLint("NewApi")
+    private void startCountAnimation() {
+        ValueAnimator animator = new ValueAnimator();
+        animator.setObjectValues(0, 727);
+        animator.setDuration(5000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator animation) {
+                txtPontosJogadorMenu.setText("" + (int) animation.getAnimatedValue());
+            }
+        });
+        animator.start();
+    }
+
 
     public void iniciarNovoJogo(View view) {
         Intent intent = new Intent(this, ActJogo.class);
